e4250a00478076b1ff043e8a8d6fc25cee34a68b
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-16 13:12:02 +0300

    Create nav component

---
+++ components/Nav/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <nav class="flex h-18 py-4 w-full m-auto text-white">
+    <NuxtLink to="/">
+      <h1
+        class="transform duration-300 hover:-translate-y-1 font-display font-thin tracking-wide m-auto pl-4 md:pl-12 text-2xl md:text-5xl hover:text-purple-500"
+      >
+        Qogita
+      </h1>
+    </NuxtLink>
+    <div
+      class="flex justify-end items-end pr-4 md:pr-24 w-full space-x-6 md:space-x-12 font-display font-thin md:font-light"
+    >
+      <div
+        role="button"
+        aria-label="Open Cart Button"
+        class="flex transition duration-300 transform hover:-translate-y-1 hover:font-normal"
+        @click="openCart"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-8 w-8 hover:text-purple-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.3"
+            d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+          />
+        </svg>
+
+        <span
+          v-if="totalItems > 0"
+          class="transform duration-300 absolute -right-2 -top-2 h-5 w-5 inline-flex items-center justify-center p-1 text-sm font-semibold text-red-800 bg-gray-100 rounded-full dark:bg-red-700 dark:text-gray-300"
+        >
+          {{ totalItems }}
+        </span>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { useShoppingStore } from '../../store/shoppingCart';
+
+export default {
+  computed: {
+    totalItems() {
+      return useShoppingStore().getTotalItems;
+    },
+  },
+  methods: {
+    openCart() {
+      useShoppingStore().openCart();
+    },
+  },
+};
+</script>
