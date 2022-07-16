e6b1986e8b9d2b7236ca401485887816475a4e3d
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-16 19:57:36 +0300

    Add dark or light class into layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="h-screen relative dark:bg-gray-900">
+  <div
+    class="h-screen relative dark:bg-gray-900"
+    :class="dark ? 'dark' : 'light'"
+  >
     <v-header></v-header>
     <hero-image></hero-image>
     <slot></slot>
+    <theme-switcher></theme-switcher>
     <v-footer></v-footer>
   </div>
 </template>
@@ -11,12 +15,21 @@
 import VHeader from '../components/Header/index.vue';
 import VFooter from '../components/Footer/index.vue';
 import HeroImage from '../components/HeroImage/index.vue';
+import ThemeSwitcher from '../components/ThemeSwitcher/index.vue';
+
+import { useThemeStore } from '../store/theme.ts';
 
 export default {
   components: {
     VHeader,
     VFooter,
     HeroImage,
+    ThemeSwitcher,
+  },
+  computed: {
+    dark() {
+      return useThemeStore().getDark;
+    },
   },
 };
 </script>
