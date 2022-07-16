af1b05e4aa8f79ac24d26ed2152e5f8a312be4ae
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-16 13:06:20 +0300

    Create hero image component

---
+++ components/HeroImage/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    className="sm:bg-center lg:bg-center bg-top bg-cover sm:bg-fixed lg:bg-fixed bg-no-repeat md:w-full sm:h-3/4 lg:h-3/4 h-2/4 bg-image"
+  />
+</template>
+
+<style scoped>
+.bg-image {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3)),
+    url('https://cdn.shopify.com/s/files/1/0191/6390/collections/banner_new-arrivals_1200x1600.jpg?v=1586155787');
+}
+</style>
