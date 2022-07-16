46c958df982545ff566ac4548a6e74124b632f29
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-16 13:05:21 +0300

    Create banner component

---
+++ components/Banner/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-gray-200 bg-opacity-90 dark:bg-gray-900">
+    <div class="max-w-7xl mx-auto py-2 px-3 sm:px-6 lg:px-8">
+      <p
+        class="text-sm md:text-base text-opacity-80 font-display text-center text-gray-900 tracking-wider font-light dark:text-gray-200"
+      >
+        Free shipping on orders over 99€!
+      </p>
+    </div>
+  </div>
+</template>
