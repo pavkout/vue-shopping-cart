e31fabc6de24fd520d2c34398136153b4f5d3fff
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-16 13:07:58 +0300

    Create item quantity component

---
+++ components/ItemQuantity/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="h-8 flex">
+    <div
+      class="h-8 w-8 border-2 rounded-full cursor-pointer flex items-center justify-center"
+      @click="subtractQuantityFromCart;"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-4 w-4"
+        fill="none"
+        view-box="0 0 24 24"
+        stroke="gray"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M20 12H4"
+        />
+      </svg>
+    </div>
+
+    <p
+      class="h-8 w-6 flex items-center justify-center font-medium text-purple-600 hover:text-purple-500 select-none"
+    >
+      {{ quantity }}
+    </p>
+
+    <div
+      class="h-8 w-8 border-2 rounded-full cursor-pointer flex items-center justify-center"
+      @click="addQuantityToCart"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-4 w-4"
+        fill="none"
+        view-box="0 0 24 24"
+        stroke="gray"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['gtin', 'quantity', 'onIncrease', 'onSubtract'],
+  computed: {},
+  methods: {
+    addQuantityToCart() {},
+    subtractQuantityFromCart() {},
+  },
+};
+</script>
