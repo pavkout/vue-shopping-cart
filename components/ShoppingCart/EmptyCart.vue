bf468d4627c0b2eb9098b7d45447d682c08881e4
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-16 13:11:31 +0300

    Create empty sshopping cart component

---
+++ components/ShoppingCart/EmptyCart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    className="flex flex-col items-center justify-center text-center pt-20 text-gray-900 dark:text-gray-200"
+  >
+    <img
+      alt="Empty Cart Image"
+      src="https://i.imgur.com/dCdflKN.png"
+      width="80"
+      height="80"
+    />
+    <h3>
+      <strong>Your Cart is Empty</strong>
+    </h3>
+    <h4>Add something to make me happy :)</h4>
+  </div>
+</template>
