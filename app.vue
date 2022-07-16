5b996636af4928c2b1d1d4fbc121904e6e0a050b
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-16 13:04:39 +0300

    Init nuxt app

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup>
+definePageMeta({
+  title: 'Pavlos Koutoglou',
+});
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+import { defineNuxtConfig } from 'nuxt';
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: ['@nuxt/typescript-build', '@pinia/nuxt'],
+  layout: 'default',
+  typescript: {
+    shim: false,
+    typeCheck: true,
+  },
+  css: ['@/assets/css/styles.css'],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+});
