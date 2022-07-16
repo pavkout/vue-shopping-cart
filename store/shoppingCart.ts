7c797abfefd2cb48c8b447ff22e65a59a1654bf6
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-16 13:16:33 +0300

    Create pinia store for state management

---
+++ store/shoppingCart.ts
@@ -0,0 +1,105 @@
+import { defineStore } from 'pinia';
+import { IState, Product } from '../types/types';
+import {
+  addProductToCart,
+  increaseProductQuantity,
+  increaseQuantity,
+  increaseTotalPrice,
+  removeProductFromCart,
+  subtractProductQuantity,
+  subtractQuantity,
+  subtractTotalPrice,
+} from '../utils/storeHelpers';
+
+const state = (): IState => ({
+  cart: [],
+  totalItems: 0,
+  totalPrice: 0,
+  isCartOpen: false,
+});
+
+const getters = {
+  getCart(state: IState) {
+    return state.cart;
+  },
+  getTotalItems(state: IState) {
+    return state.totalItems;
+  },
+  getTotalPrice(state: IState) {
+    return state.totalPrice;
+  },
+  getIsCartOpen(state: IState) {
+    return state.isCartOpen;
+  },
+};
+
+const actions = {
+  addToCart(product: Product) {
+    this.cart = addProductToCart(this.cart, product);
+    this.totalItems = increaseQuantity(this.totalItems, product.quantity);
+    this.totalPrice = increaseTotalPrice(
+      this.totalPrice,
+      product.recommendedRetailPrice,
+      product.quantity
+    );
+  },
+  removeFromCart(gtin: string) {
+    // Find the product in order to get the quantity.
+    const product: Product | undefined = this.cart.find((p) => p.gtin === gtin);
+
+    this.cart = removeProductFromCart(this.cart, gtin);
+    this.totalItems = subtractQuantity(this.totalItems, product?.quantity);
+    this.totalPrice = subtractTotalPrice(
+      this.totalPrice,
+      product?.recommendedRetailPrice,
+      product?.quantity
+    );
+  },
+  increaseCartQuantity(gtin: string) {
+    // Find the product in order to get the quantity.
+    const product = this.cart.find((p) => p.gtin === gtin);
+
+    this.cart = increaseProductQuantity(this.cart, gtin);
+    this.totalItems = increaseQuantity(this.totalItems);
+    this.totalPrice = increaseTotalPrice(
+      this.totalPrice,
+      product?.recommendedRetailPrice
+    );
+  },
+  subtractCartQuantity(gtin: string) {
+    // Find the product in order to get the quantity.
+    let product = this.cart.find((p) => p.gtin === gtin);
+
+    this.cart = subtractProductQuantity(this.cart, gtin);
+    this.totalItems = subtractQuantity(this.totalItems);
+    this.totalPrice = subtractTotalPrice(
+      this.totalPrice,
+      product?.recommendedRetailPrice
+    );
+  },
+  resetCart() {
+    this.cart = [];
+    this.totalItems = 0;
+    this.totalPrice = 0;
+    this.isCartOpen = false;
+  },
+  openCart() {
+    this.isCartOpen = true;
+  },
+  closeCart() {
+    this.isCartOpen = false;
+  },
+  initCart(state: IState) {
+    this.cart = state.cart;
+    this.totalItems = state.totalItems;
+    this.totalPrice = state.totalPrice;
+    this.isCartOpen = state.isCartOpen;
+  },
+};
+
+export const useShoppingStore = defineStore({
+  id: 'shoppingStore',
+  state,
+  getters,
+  actions,
+});
