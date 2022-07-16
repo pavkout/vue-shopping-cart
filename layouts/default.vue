8b1da323cbb0d6ca60f1e1e6146375c59cbcff74
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-16 13:14:24 +0300

    Overide default layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="h-screen relative dark:bg-gray-900">
+    <v-header></v-header>
+    <hero-image></hero-image>
+    <slot></slot>
+    <v-footer></v-footer>
+  </div>
+</template>
+
+<script>
+import VHeader from '../components/Header/index.vue';
+import VFooter from '../components/Footer/index.vue';
+import HeroImage from '../components/HeroImage/index.vue';
+
+export default {
+  components: {
+    VHeader,
+    VFooter,
+    HeroImage,
+  },
+};
+</script>
