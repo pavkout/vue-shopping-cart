253c9b132e85d5573b348e20bd1604f4434ede02
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-16 13:05:59 +0300

    Create header component

---
+++ components/Header/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="absolute w-full z-10">
+    <v-banner></v-banner>
+    <v-nav></v-nav>
+  </div>
+</template>
+
+<script setup>
+import VBanner from '../Banner/index.vue';
+import VNav from '../Nav/index.vue';
+</script>
